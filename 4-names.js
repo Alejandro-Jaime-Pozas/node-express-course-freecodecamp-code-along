83097572a1843e4478e8f42bb075a57090c3bb87
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2022-10-18 11:51:28 -0500

    first

---
+++ 4-names.js
@@ -0,0 +1,6 @@
+const secret = 'Super Secret'
+const peter = 'peter'
+const john = 'john'
+
+// this logs out the module on terminal, with exports: {} empty obj, you can add to this
+console.log(module);
---
+++ app.js
@@ -0,0 +1,9 @@
+// Node uses CommonJS under the hood, every file in Node is a module (by default)
+// modules: encapsulated code (only share minimum)
+
+
+
+
+sayHi('Susan')
+sayHi(john)
+sayHi(peter)
