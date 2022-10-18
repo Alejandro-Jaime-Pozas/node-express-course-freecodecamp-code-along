cce4297ff8a86f8c643a93583418d055b7fec863
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2022-10-18 11:55:15 -0500

    added 3 modules

--- 04-names.js
+++ 04-names.js
@@ -1,6 +1,10 @@
+// local
 const secret = 'Super Secret'
+// share with rest of app
 const peter = 'peter'
 const john = 'john'
+
+module.exports = { john, peter }
 
 // this logs out the module on terminal, with exports: {} empty obj, you can add to this
 console.log(module);
