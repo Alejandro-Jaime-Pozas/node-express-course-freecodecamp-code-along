7b6c1f3120efeba712f8df8895f3870156483a8d
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2022-11-07 10:31:57 -0600

    installs

--- app.js
+++ app.js
@@ -15,7 +15,7 @@
 
 // nodemon checks the files in code, and restarts the npm project automatically
 
-// npm install nodemon -D (installs nodemon as a devDependency in package.json for use only in production)
+// npm install nodemon -D or --save-dev (installs nodemon as a devDependency in package.json for use only in development phase, not deployment)
 
 // add to "scripts" key in package.json a "start" key with value "nodemon app.js" to be able to type and run the app with npm start
 
@@ -24,4 +24,4 @@ const _ = require('lodash') // common convention to call variable _
 const items = [1,[2,[3,[4]]]]
 const newItems = _.flattenDeep(items)
 console.log(newItems);
-console.log("hello people");
+console.log("hello world");
